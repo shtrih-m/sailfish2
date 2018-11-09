3557e5a41adda80f3c34a7de80b560a68dd78465
kravtsov@shtrih-m.ru <kravtsov@shtrih-m.ru>
2018-11-09 14:20:29 +0300

    [-] Поправил версию

--- libs/qsmfptr/src/shtrihfiscalprinter.cpp
+++ libs/qsmfptr/src/shtrihfiscalprinter.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -5464,7 +5464,7 @@ int ShtrihFiscalPrinter::printImage(PrinterImage image)
 
 QString ShtrihFiscalPrinter::getVersion()
 {
-    return "1.5";
+    return "1.6";
 }
 
 int ShtrihFiscalPrinter::printImage(int startLine, int endLine)
